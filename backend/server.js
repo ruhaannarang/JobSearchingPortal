80b07e12b58856769a6919801c2a42bdd53a78ee
Ruhaan Narang <techieruhaan@gmail.com>
2026-08-15 23:15:32 +0530

    email fix try 2

--- backend/server.js
+++ backend/server.js
@@ -461,12 +461,15 @@ const getTransporter = async () => {
     process.env.EMAIL_PASS !== "your_app_password"
   ) {
     cachedTransporter = nodemailer.createTransport({
-      service: process.env.EMAIL_SERVICE || "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
     return cachedTransporter;
   }
 
